51d33d9e6c0b7b8a31323793c2c59795f6452ebd
barnabypekinstores-Git <barnabypekinstores@gmail.com>
2026-01-04 16:35:27 +1100

    fixed error

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const suburbs = [
+const sydneySuburbs = [
     {name: "Sydney CBD", lat: -33.8688, lng: 151.2093},
     {name: "Surry Hills", lat: -33.887, lng: 151.213},
     {name: "Newtown", lat: -33.898, lng: 151.178}
