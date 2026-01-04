8d3544783b568a980d194129828afa9806890a75
barnabypekinstores-Git <barnabypekinstores@gmail.com>
2026-01-04 15:18:54 +1100

    Initial commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const suburbs = [
+    {name: "Sydney CBD", lat: -33.8688, lng: 151.2093},
+    {name: "Surry Hills", lat: -33.887, lng: 151.213},
+    {name: "Newtown", lat: -33.898, lng: 151.178}
+];
+
+console.log('✅ data.js loaded with', suburbs.length, 'suburbs');
